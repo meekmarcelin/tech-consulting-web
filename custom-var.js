509d21eb374e8396795f27a5ac637a884c0c3644
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-08-05 20:44:11 +0200

    commit web

---
+++ custom-var.js
@@ -0,0 +1,3 @@
+var GOOGLE_ANALYTICS_ID = "G-XGJF1MDNB2"; // example "UA-XXXXXXXX-X"
+var CRISP_WEBSITE_ID = "4a679ab8-d27d-48a2-9b5a-b7c35f69af9b";    // example "9x5x-25x-4x67-x065-x91xxxxx6x83"
+var FORMSPREE_EMAIL = "meekmarcelin@gmail.com";     // example meekmarcelin@gmail.com
